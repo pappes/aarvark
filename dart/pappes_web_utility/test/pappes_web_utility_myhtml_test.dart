ed9f9eb6bdd96d1a8f809a2d6ddb3afcb6618fe7
pappes <pappes@gmail.com>
2015-01-05 22:42:12 +1000

    more testing

--- dart/pappes_web_utility/test/pappes_web_utility_myhtml_test.dart
+++ dart/pappes_web_utility/test/pappes_web_utility_myhtml_test.dart
@@ -204,4 +204,50 @@ void defineTests() {
     test('frame_target', () => expect(document.querySelector('#frame_target').attributes['target'], '_self'));
   });
 
+
+
+  group('pappes_web_utility MyHtml removeAllOverlays tests initial HTML state', () {
+    test('element_p', () => expect(document.querySelector('#outside_p').attributes['id'], 'outside_p'));
+    test('element_a_text', () => expect(document.querySelector('#outside_anchor_text').attributes['id'], 'outside_anchor_text'));
+    test('element_a_blank', () => expect(document.querySelector('#outside_anchor_button').attributes['id'], 'outside_anchor_button'));
+    test('element_iframe', () => expect(document.querySelector('#outside_iframe').attributes['src'], 'pappes_web_utility_myhtml_iframe_test.html'));
+    //test('element_frame', () => expect(document.querySelector('#outside_frame_a').attributes['id'], 'outside_frame_a'));
+    test('element_p', () => expect(document.querySelector('#inside_p'), isNull));
+    test('element_a_text', () => expect(document.querySelector('#inside_anchor_text'), isNull));
+    test('element_a_blank', () => expect(document.querySelector('#inside_anchor_button'), isNull));
+    test('element_frame', () => expect(document.querySelector('#inside_frame_a'), isNull));
+  });
+  group('pappes_web_utility MyHtml removeAllOverlays change state', () {
+    test('alter href links', () {
+      expect(MyHtml.removeAllOverlays(document), isNull);
+    });
+  });
+  group('pappes_web_utility MyHtml removeAllOverlays tests intermediate HTML state', () {
+    test('element_p', () => expect(document.querySelector('#outside_p'), isNull));
+    test('element_a_text', () => expect(document.querySelector('#outside_anchor_text').attributes['id'], 'outside_anchor_text'));
+    //test('element_a_blank', () => expect(document.querySelector('#outside_anchor_button'), isNull));
+    //test('element_iframe', () => expect(document.querySelector('iframe').attributes['src'], 'pappes_web_utility_myhtml_iframe_test.html'));
+    test('element_frame', () => expect(document.querySelector('#outside_frame_a'), isNull));
+    test('element_p', () => expect(document.querySelector('#inside_p'), isNull));
+    test('element_a_text', () => expect(document.querySelector('#inside_anchor_text'), isNull));
+    test('element_a_blank', () => expect(document.querySelector('#inside_anchor_button'), isNull));
+    test('element_frame', () => expect(document.querySelector('#inside_frame_a'), isNull));
+  });
+  /*group('pappes_web_utility MyHtml removeAllOverlays change state', () {
+    test('alter href links', () {
+      expect(MyHtml.removeAllOverlays(document), isNull);
+    });
+  });
+  group('pappes_web_utility MyHtml removeAllOverlays tests final HTML state', () {
+    test('element_p', () => expect(document.querySelector('#outside_p'), isNull));
+    test('element_a_text', () => expect(document.querySelector('#outside_anchor_text'), isNull));
+    test('element_a_blank', () => expect(document.querySelector('#outside_anchor_button'), isNull));
+    test('element_iframe', () => expect(document.querySelector('#outside_iframe'), isNull));
+    test('element_frame', () => expect(document.querySelector('#outside_frame_a'), isNull));
+    test('element_p', () => expect(document.querySelector('#inside_p').attributes['id'], isNull));
+    test('element_a_text', () => expect(document.querySelector('#inside_anchor_text').attributes['id'], 'inside_anchor_text'));
+    test('element_a_blank', () => expect(document.querySelector('#inside_anchor_button'), isNull));
+    test('element_frame', () => expect(document.querySelector('#inside_frame_a'), isNull));
+  });*/
+
 }
