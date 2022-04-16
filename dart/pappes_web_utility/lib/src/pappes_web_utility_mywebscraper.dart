140eb1c2f3c6b14fdb62d0158270730baa2ea26d
pappes <pappes@gmail.com>
2022-04-16 14:05:35 +1000

    debugging

--- dart/pappes_web_utility/lib/src/pappes_web_utility_mywebscraper.dart
+++ dart/pappes_web_utility/lib/src/pappes_web_utility_mywebscraper.dart
@@ -39,14 +39,14 @@ class MyWebScraper {
       ],
     );
 
-    log.info('Function : scrapeIMDB, found : {[map,${retval.toString()}]}');
+    log.fine('Function : scrapeIMDB, found : {[map,${retval.toString()}]}');
     return retval;
   }
 
   /// Pull details from an anywhere search result web page into a map.
   ///
   static List<Map> scrapeAnywhereList() {
-    log.info('Function : scrapeAnywhereList');
+    log.info('Function : scrapeAnywhereList V 0.10');
     final retval = <Map>[];
 
     final jsonList = getElementsList(
@@ -55,35 +55,44 @@ class MyWebScraper {
     );
     var resultIndex = 1;
     for (final resultJson in jsonList) {
-      log.info(
+      log.finest(
           'Function : scrapeAnywhereList, found : {[encoded json.InnerHtml ,${resultJson.innerHtml.toString()}]}');
-      final result = jsonDecode(resultJson.innerHtml);
-      log.info(
-          'Function : scrapeAnywhereList, found : {[decoded json,${result.toString()}]}');
-      retval.add(summariseAnywhereShow(resultIndex++, result));
+      var result;
+      try {
+        result = jsonDecode(resultJson.innerHtml);
+      } catch (ignore) {}
+      if (result != null) {
+        log.finest(
+            'Function : scrapeAnywhereList, found : {[decoded json,${result.toString()}]}');
+        retval.addAll(summariseAnywhereShows(resultIndex++, result));
+      }
     }
 
-    log.info(
+    log.fine(
         'Function : scrapeAnywhereList, found : {[list,${retval.toString()}]}');
     return retval;
   }
 
-  static Map summariseAnywhereShows(int index, Object results) {
+  static List<Map> summariseAnywhereShows(int index, Object results) {
     log.info('Function : summariseAnywhereShows');
     if (results is Map) {
-      return summariseAnywhereShow(index, results);
+      return [summariseAnywhereShow(index, results)];
     } else if (results is Iterable) {
+      log.finest(
+          'Function : summariseAnywhereShows, number found : ${results.length}');
+      final retval = <Map>[];
       for (final result in results) {
         if (result is Map) {
-          return summariseAnywhereShow(index, result);
+          retval.add(summariseAnywhereShow(index, result));
         } else if (result is Iterable) {
           return summariseAnywhereShows(index, result);
         }
       }
+      return retval;
     }
-    log.info(
+    log.fine(
         'Function : summariseAnywhereShow, found : {[unexpected json contents,${results.toString()}]}');
-    return {};
+    return [];
   }
 
   static Map summariseAnywhereShow(int index, Map resultMap) {
@@ -94,32 +103,81 @@ class MyWebScraper {
       retval["name"] = resultMap["name"];
       retval["description"] = resultMap["description"];
       retval["link"] = resultMap["url"];
-      retval["startTime"] = fmt.format(resultMap["startDate"]);
-      retval["endTime"] = fmt.format(resultMap["endDate"]);
-      retval["duration"] = getDurationText(
-        resultMap["startDate"],
-        resultMap["endDate"],
-      );
-      retval["venue"] = resultMap["location"]["name"];
-      retval["suburb"] = resultMap["location"]["addressLocality"];
-      retval["postcode"] = resultMap["location"]["postalCode"];
+      retval["venue"] = resultMap["location"]?["name"];
+      retval["suburb"] = resultMap["location"]?["address"]?["addressLocality"];
+      retval["postcode"] = resultMap["location"]?["address"]?["postalCode"];
       retval["producer"] = resultMap["organizer"]["name"];
-      var price = -1;
-      for (final ticketCat in resultMap["offers"]) {
-        if (ticketCat["availability"] == "InStock" &&
-            ticketCat["price"] > price &&
-            ticketCat["validThrough"] < DateTime.now()) {
-          price = ticketCat["price"];
+      retval["price"] = -1.0;
+      if (resultMap["offers"] is Map)
+        getAnywhereOffer(resultMap, retval);
+      else {
+        for (final ticketCat in resultMap["offers"]) {
+          getAnywhereOffer(ticketCat, retval);
         }
       }
     }
-    log.info(
+    log.fine(
         'Function : summariseAnywhereShow, found : {[map,${retval.toString()}]}');
 
     return retval;
   }
 
+  static void getAnywhereOffer(Map offer, Map result) {
+    log.info('Function : getAnywhereOffer, offer : ${offer.toString()}');
+    // Grab max price to avoid concession prices.  May accidentally grab VIP price.
+    final maxPrice = double.parse(result["price"]);
+    log.finest('Function : getAnywhereOffer, maxPrice : $maxPrice');
+    final offerPrice = double.parse(offer["price"]);
+    log.finest('Function : getAnywhereOffer, offerPrice : $offerPrice');
+    final validThrough = DateTime.parse(offer["validThrough"]);
+    log.finest('Function : getAnywhereOffer, validThrough : $validThrough');
+    final availability = DateTime.parse(offer["availability"]);
+    log.finest('Function : getAnywhereOffer, availability : $availability');
+    if (availability == "InStock" &&
+        offerPrice > maxPrice &&
+        validThrough.isAfter(DateTime.now())) {
+      log.finest('Function : getAnywhereOffer, value : $offerPrice');
+      result["Price"] = offerPrice;
+    }
+    log.finest('Function : getAnywhereOffer, get duration');
+
+    // Session time is hidden in one of the offers!
+    if (offer.containsKey("startTime") && offer.containsKey("endTime")) {
+      final startDate = DateTime.parse(offer["startTime"]);
+      final endDate = DateTime.parse(offer["endTime"]);
+      result["startTime"] = fmt.format(startDate);
+      result["endTime"] = fmt.format(endDate);
+      result["duration"] = getDurationText(
+        startDate,
+        endDate,
+      );
+      log.fine(
+          'Function : getAnywhereOffer, found : {[map,${result.toString()}]}');
+    }
+  }
+
+  static void getAnywhereDuration(Map event, Map result) {
+    log.info('Function : getAnywhereDuration, event : ${event.toString()}');
+
+    // Session time is hidden in one of the offers!
+    if (event.containsKey("startTime") && event.containsKey("endTime")) {
+      final startDate = DateTime.parse(event["startTime"]);
+      log.finest('Function : getAnywhereOffer, startDate : $startDate');
+      final endDate = DateTime.parse(event["endTime"]);
+      log.finest('Function : getAnywhereOffer, endDate : $endDate');
+      result["startTime"] = fmt.format(startDate);
+      result["endTime"] = fmt.format(endDate);
+      result["duration"] = getDurationText(
+        startDate,
+        endDate,
+      );
+      log.fine(
+          'Function : getAnywhereDuration, found : {[map,${result.toString()}]}');
+    }
+  }
+
   static String? getDurationText(DateTime start, DateTime end) {
+    log.info('Function : getDurationText');
     try {
       final totalMinutes = end.difference(start).inMinutes;
       final hours = (totalMinutes / 60).round();
